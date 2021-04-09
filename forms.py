62cc3a3e6da121223981afd63d3c19f515992e57
Emily McMullan <eemcmullan92@gmail.com>
2021-04-08 21:09:33 -0400

    added registration form

--- forms.py
+++ forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import Length, DataRequired, Email
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Length, DataRequired, EqualTo
 
 class RegistrationForm(FlaskForm):
     # username must be between 5 and 15 characters
     username =StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
-    
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import json
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'QUWU7Ax94jCsknrT'
+
+
+
 with open('presets.json', 'r') as input:
     preset_data = json.load(input)
 
@@ -15,6 +19,11 @@ def home():
 @app.route("/presets")
 def json_dump():
     return render_template('presets.html', title='Base Data', preset_data = preset_data)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    reg_form = RegistrationForm()
+    return render_template('register.html', title='Register', form=reg_form)
 
 # run on debug mode to not re-start server after changes
 if __name__ == '__main__':
